2a20b2192d473ced84defed5b94888188d997b04
LeanneDobson <leanne.banns@hotmail.co.uk>
2018-09-21 13:44:26 +0100

    Altered to allow for more states

--- js/colourchange.js
+++ js/colourchange.js
@@ -1,14 +1,21 @@
-  clicked = true;
+    var color = 1;
+
     $(document).ready(function(){
-        $("button").click(function(){
-            if(clicked){
+        $("button").click(function(){     
+            if (color === 1) {
                 $('header').css('background-color', '#9C27B0');
                 $(this).css('color', '#9C27B0');
-                clicked  = false;
-            } else {
+                color = 2;
+            }  else if (color === 2) {
                 $('header').css('background-color', '#009688');
                 $(this).css('color', '#009688');
-                clicked  = true;
-            } 
+                color = 3;
+            } else {
+                $('header').css('background-color', '#ef3340');
+                $(this).css('color', '#ef3340');
+                color = 1;
+            }
         });
     });
+
+
